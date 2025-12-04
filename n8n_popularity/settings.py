254956fa29fab4485eb9ec8e4c431e40c1e746df
Vivekananda69 <vivekanandabandapu1@gmail.com>
2025-12-04 13:40:51 +0530

    Added PostgreSQL support

--- n8n_popularity/settings.py
+++ n8n_popularity/settings.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 import os
 from dotenv import load_dotenv
+import dj_database_url
 
 # ===========================
 # PATHS
@@ -80,12 +81,24 @@ WSGI_APPLICATION = 'n8n_popularity.wsgi.application'
 # DATABASE
 # Railway: use SQLite OR PostgreSQL
 # ===========================
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / "db.sqlite3",
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+if DATABASE_URL:
+    DATABASES = {
+        "default": dj_database_url.parse(
+            DATABASE_URL,
+            conn_max_age=600,
+            ssl_require=False
+        )
     }
-}
+else:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "db.sqlite3",
+        }
+    }
+
 
 # For future PostgreSQL migration:
 # if os.getenv("DATABASE_URL"):
